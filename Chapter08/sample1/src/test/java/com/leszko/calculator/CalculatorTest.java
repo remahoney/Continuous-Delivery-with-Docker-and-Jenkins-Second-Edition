d8a5e5ae56940824fd1f54c952aa891e92e98bf6
Rick Mahoney <71021527+remahoney@users.noreply.github.com>
2025-02-16 18:07:28 -0500

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,10 +1,12 @@
 /**
-This is the calculator JavaScript file for Exercise 4, Problem 2
+* This is the calculator JavaScript file for Exercise 4, Problem 2
 */
 package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-
+/**
+* This is the  public class
+*/
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
