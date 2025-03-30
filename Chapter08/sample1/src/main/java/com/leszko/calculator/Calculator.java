ae1afe0a696caddbd1d365c364c11177d2d33d1f
Rick Mahoney <71021527+remahoney@users.noreply.github.com>
2025-03-30 16:56:44 -0400

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -1,9 +1,15 @@
+/**
+* start of calculator
+*/
 package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
+/**
+* Inserting javadoc comment
+*/
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
